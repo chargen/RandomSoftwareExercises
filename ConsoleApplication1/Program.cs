9133ff986a38175bfc2e6488b44114e2fbb37f1e
ehelin <ehelin@felpsis.net>
2016-04-18 23:07:04 -0500

    some more random exercises

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        bool cont = true;
+        int ctr = 1;
+        int maxRowLength = n;
+
+        while (cont)
+        {
+            PrintStairCaseRow(ctr, maxRowLength);
+
+            if (ctr >= n)
+                break;
+            else
+                ctr++;
+        }
+    }
+
+    private static void PrintStairCaseRow(int currentRow, int maxRowLength)
+    {
+        string row = string.Empty;
+        int ctr = 0;
+        int numOfSymbols = currentRow + 1;
+        int numOfSpaces = maxRowLength - numOfSymbols;
+
+        while (ctr < maxRowLength)
+        {
+            if (ctr <= numOfSpaces)
+                row += " ";
+            else
+                row += "#";
+
+            ctr++;
+        }
+
+        Console.WriteLine(row);
+    }
+    //static void Main(String[] args)
+    //{
+    //    bool cont = true;
+
+    //    while (cont)
+    //    {
+    //        int maxRowLength = Convert.ToInt32(Console.ReadLine());
+    //        int ctr = 1;
+
+    //        while (ctr <= maxRowLength)
+    //        {
+    //            PrintStairCaseRow(ctr, maxRowLength);
+    //            ctr++;
+    //        }
+    //    }
+    //}
+
+    //private static void PrintStairCaseRow(int currentRow, int maxRowLength)
+    //{
+    //    string row = string.Empty;
+    //    int ctr = 0;
+    //    int numOfSymbols = currentRow + 1;
+    //    int numOfSpaces = maxRowLength - numOfSymbols;
+
+    //    while (ctr < maxRowLength)
+    //    {
+    //        if (ctr <= numOfSpaces)
+    //            row += " ";
+    //        else
+    //            row += "#";
+
+    //        ctr++;
+    //    }
+
+    //    Console.WriteLine(row);
+    //}
+}
--- RandomSoftwareExercises/Program.cs
+++ RandomSoftwareExercises/Program.cs
@@ -15,5 +15,72 @@ namespace RandomSoftwareExercises
             Main m = new Main();
             m.Run();
         }
+
+        /// <summary>
+        /// print number of positive, negative and zero numbers in the provided array
+        /// </summary>
+        private static void RunExercise()
+        {
+            int n = 6;
+            string[] arr_temp = "-4 3 -9 0 4 1".Split(' ');
+            int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+
+            double positiveNumbers = 0;
+            double negativeNumbers = 0;
+            double zeroNumbers = 0;
+
+            foreach (int arrValue in arr)
+            {
+                if (arrValue > 0)
+                    positiveNumbers++;
+                else if (arrValue < 0)
+                    negativeNumbers++;
+                else
+                    zeroNumbers++;
+            }
+
+            Console.WriteLine((positiveNumbers / n).ToString());
+            Console.WriteLine((negativeNumbers / n).ToString());
+            Console.WriteLine((zeroNumbers / n).ToString());
+        }
+
+        /// <summary>
+        /// print out stair symbols for supplied number
+        /// </summary>
+        private static void RunExercise2()
+        {
+            bool cont = true;
+
+            while (cont)
+            {
+                int maxRowLength = Convert.ToInt32(Console.ReadLine());
+                int ctr = 1;
+
+                while (ctr <= maxRowLength)
+                {
+                    PrintStairCaseRow(ctr, maxRowLength);
+                    ctr++;
+                }
+            }
+        }
+        private static void PrintStairCaseRow(int currentRow, int maxRowLength)
+        {
+            string row = string.Empty;
+            int ctr = 0;
+            int numOfSymbols = currentRow + 1;
+            int numOfSpaces = maxRowLength - numOfSymbols;
+
+            while (ctr < maxRowLength)
+            {
+                if (ctr <= numOfSpaces)
+                    row += " ";
+                else
+                    row += "#";
+
+                ctr++;
+            }
+
+            Console.WriteLine(row);
+        }
     }
 }
